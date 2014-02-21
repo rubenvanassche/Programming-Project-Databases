2a9138b8b54d74f2b28abab114a0b801ef1c4b18
kristof <kristof.de.middelaer@gmail.com>
2014-02-21 19:22:42 +0100

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+-- Host: localhost
+-- Generation Time: Feb 21, 2014 at 06:27 
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 --
 -- Database: `coachcenter`
 --
@@ -8,13 +16,15 @@
 -- Table structure for table `cards`
 --
 
-CREATE TABLE `cards` (
+CREATE TABLE IF NOT EXISTS `cards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `player_id` int(11) NOT NULL,
   `match_id` int(11) NOT NULL,
   `color` int(11) NOT NULL,
   `time` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `player_ids` (`player_id`),
+  UNIQUE KEY `match` (`match_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -23,7 +33,7 @@ CREATE TABLE `cards` (
 -- Table structure for table `competition`
 --
 
-CREATE TABLE `competition` (
+CREATE TABLE IF NOT EXISTS `competition` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
   PRIMARY KEY (`id`)
@@ -35,7 +45,7 @@ CREATE TABLE `competition` (
 -- Table structure for table `continent`
 --
 
-CREATE TABLE `continent` (
+CREATE TABLE IF NOT EXISTS `continent` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
@@ -47,11 +57,12 @@ CREATE TABLE `continent` (
 -- Table structure for table `country`
 --
 
-CREATE TABLE `country` (
+CREATE TABLE IF NOT EXISTS `country` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
   `continent_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `continent` (`continent_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -60,14 +71,17 @@ CREATE TABLE `country` (
 -- Table structure for table `goal`
 --
 
-CREATE TABLE `goal` (
+CREATE TABLE IF NOT EXISTS `goal` (
   `id` int(11) NOT NULL,
   `match_id` int(11) NOT NULL,
   `time` tinyint(4) NOT NULL,
   `player_id` int(11) NOT NULL,
   `team_id` int(11) NOT NULL,
   `penaltyphase` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `match` (`match_id`),
+  UNIQUE KEY `player` (`player_id`),
+  UNIQUE KEY `team` (`team_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -76,14 +90,18 @@ CREATE TABLE `goal` (
 -- Table structure for table `match`
 --
 
-CREATE TABLE `match` (
+CREATE TABLE IF NOT EXISTS `match` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `hometeam_id` int(11) NOT NULL,
   `awayteam_id` int(11) NOT NULL,
   `competition_id` int(11) NOT NULL,
   `lineup_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `hometeam` (`hometeam_id`),
+  UNIQUE KEY `awayteam` (`awayteam_id`),
+  UNIQUE KEY `competition` (`competition_id`),
+  UNIQUE KEY `lineup` (`lineup_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -92,7 +110,7 @@ CREATE TABLE `match` (
 -- Table structure for table `player`
 --
 
-CREATE TABLE `player` (
+CREATE TABLE IF NOT EXISTS `player` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
   PRIMARY KEY (`id`)
@@ -104,7 +122,7 @@ CREATE TABLE `player` (
 -- Table structure for table `playerPerMatch`
 --
 
-CREATE TABLE `playerPerMatch` (
+CREATE TABLE IF NOT EXISTS `playerPerMatch` (
   `player_id` int(11) NOT NULL,
   `match_id` int(11) NOT NULL,
   `intime` tinyint(4) NOT NULL,
@@ -118,7 +136,7 @@ CREATE TABLE `playerPerMatch` (
 -- Table structure for table `playerPerTeam`
 --
 
-CREATE TABLE `playerPerTeam` (
+CREATE TABLE IF NOT EXISTS `playerPerTeam` (
   `player_id` int(11) NOT NULL,
   `team_id` int(11) NOT NULL,
   PRIMARY KEY (`player_id`,`team_id`)
@@ -130,11 +148,12 @@ CREATE TABLE `playerPerTeam` (
 -- Table structure for table `team`
 --
 
-CREATE TABLE `team` (
+CREATE TABLE IF NOT EXISTS `team` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) NOT NULL,
   `country_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `country` (`country_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -143,8 +162,55 @@ CREATE TABLE `team` (
 -- Table structure for table `teamPerCompetition`
 --
 
-CREATE TABLE `teamPerCompetition` (
+CREATE TABLE IF NOT EXISTS `teamPerCompetition` (
   `team_id` int(11) NOT NULL,
   `competition_id` int(11) NOT NULL,
-  PRIMARY KEY (`team_id`,`competition_id`)
+  PRIMARY KEY (`team_id`,`competition_id`),
+  KEY `tpc_competition` (`competition_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cards`
+--
+ALTER TABLE `cards`
+  ADD CONSTRAINT `match` FOREIGN KEY (`match_id`) REFERENCES `match` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `player` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `country`
+--
+ALTER TABLE `country`
+  ADD CONSTRAINT `continent` FOREIGN KEY (`continent_id`) REFERENCES `continent` (`id`);
+
+--
+-- Constraints for table `goal`
+--
+ALTER TABLE `goal`
+  ADD CONSTRAINT `goal_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `goal_match` FOREIGN KEY (`match_id`) REFERENCES `match` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `goal_player` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `match`
+--
+ALTER TABLE `match`
+  ADD CONSTRAINT `match_competition` FOREIGN KEY (`competition_id`) REFERENCES `competition` (`id`),
+  ADD CONSTRAINT `awayteam` FOREIGN KEY (`awayteam_id`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `hometeam` FOREIGN KEY (`hometeam_id`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `team`
+--
+ALTER TABLE `team`
+  ADD CONSTRAINT `team_country` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`);
+
+--
+-- Constraints for table `teamPerCompetition`
+--
+ALTER TABLE `teamPerCompetition`
+  ADD CONSTRAINT `tpc_competition` FOREIGN KEY (`competition_id`) REFERENCES `competition` (`id`);
+
