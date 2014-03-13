bade263690079870edb6f6819a2342db65acb1dd
Stijn-Flipper <w.stijn@gmail.com>
2014-03-13 17:11:36 +0100

    completed rudymentary crawler

--- lib/crawlers/crawler.php
+++ lib/crawlers/crawler.php
@@ -122,15 +122,75 @@ class Crawler {
         } // end if-else
     }
 
+    /**
+     * @brief Get all the players
+     *
+     * @param base_url The base_url
+     * @param rel_url The relative url
+     * @param selector The selector to be used for getting the teams.
+     * @param time_limit The maximum amount of time to be elapsed for crawling
+     * @param elapsed The time already elapsed
+     * @param visited The (relative) urls already visited.
+     */
+    public function players( $base_url, $rel_url, $selector, $time_limit, &$elapsed=0, &$visited=array()) {
+        $visited[] = $rel_url;
+
+        $start = time();
+
+        $page = new simple_html_dom();
+        $page->load_file( $base_url . $rel_url );
+
+        foreach ( $page->find( $selector ) as $player ) {
+            print trim( $team->plaintext ) . "\n";
+        } // end foreach
+
+        // stop if over time
+        $elapsed += time() - $start;
+        if ( $elapsed >= $time_limit ) {
+            $page->clear();
+            return;
+        } else {
+            // visit subpages
+            $links = $page->find( "a" );
+            $page->clear();
+
+            foreach ( $links as $link ) {
+                // only use relative links, so you're sure you will stay on
+                // the original site. Also ensure that you haven't visited
+                // the page already
+                if ( !( substr( $link->href, 0, 1) === '/' ) || in_array( $link->href, $visited ) ) continue;
+
+                $this->teams( $base_url, $link->href, $selector, $time_limit, $elapsed, $visited);
+
+                // stop if over time
+                if ( $elapsed >= $time_limit ) break;
+            } // end foreach
+
+            return;
+        } // end if-else
+    }
+
+
 }
 
 $crawler = new Crawler();
-//$crawler->teams("http://int.soccerway.com", "/", ".team a", 5);
+print "TEAMS\n";
+print "-------\n";
+$crawler->teams("http://int.soccerway.com", "/", ".team a", 5);
+print "\n";
 
+print "MATCHES\n";
+print "-------\n";
 $selectors = array(
     "match" => "table.matches tbody tr.match",
     "team1" => "td.team-a a",
     "team2" => "td.team-b a",
     "score" => "td.score a",
     "time" => "td.status a span.timestamp" );
-$crawler->matches("http://int.soccerway.com", "/", $selectors, 10);
+$crawler->matches("http://int.soccerway.com", "/", $selectors, 5);
+print "\n";
+
+print "PLAYERS\n";
+print "-------\n";
+$crawler->players("http://int.soccerway.com", "/", "table tbody td.player a", 5);
+print "\n";
