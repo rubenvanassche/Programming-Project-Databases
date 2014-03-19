de5d097d642e74c6db74d543b86697ca856ae372
Tom Roels <tom.roels94@gmail.com>
2014-03-19 20:18:15 +0100

    cleaned score prediction via match model

--- app/models/Match.php
+++ app/models/Match.php
@@ -12,6 +12,26 @@ class Match {
 		$results = DB::select('SELECT * FROM `match` WHERE id = ?', array(0));
 		return $results;
 	}
+
+	public static function getMatchByID($matchID) {
+		$results = DB::select('SELECT * FROM `match` WHERE id = ?', array($matchID));
+		return $results;
+	}
+
+	public static function getMatchByTeams($hometeam_id, $awayteam_id) {
+		$results = DB::select('SELECT * FROM `match` WHERE hometeam_id = ? AND awayteam_id = ?', array($hometeam_id, $awayteam_id);
+		return $results;
+	}
+
+	public static function getMatchByHometeam($hometeam_id) {
+		$results = DB::select('SELECT * FROM `match` WHERE hometeam_id = ?', array($hometeam_id));
+		return $results;
+	}
+
+		public static function getMatchByAwayteam($awayteam_id) {
+		$results = DB::select('SELECT * FROM `match` WHERE awayteam_id = ?', array($awayteam_id));
+		return $results;
+	}
 }
 
 ?>
