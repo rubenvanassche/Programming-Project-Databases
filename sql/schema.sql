dc678fc0fb9f9aa1e36bfe1c7e8c0a2c5a502c46
Ruben Van Assche <rubenvanassche@gmail.com>
2014-04-25 11:13:04 +0200

    user groups schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -259,6 +259,27 @@ CREATE TABLE IF NOT EXISTS `bet` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Table structure for table `userGroup`
+--
+
+CREATE TABLE `userGroup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Table structure for table `userPerUserGroup`
+--
+
+CREATE TABLE `userPerUserGroup` (
+  `user_id` int(11) NOT NULL,
+  `userGroup_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`userGroup_id`),
+  KEY `userGroup_id` (`userGroup_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
