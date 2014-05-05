2791dbdd38cedda7de7c0d3fed1fe5adbcb1a0bf
Tom Roels <tom.roels94@gmail.com>
2014-05-05 11:52:24 +0200

    Changed default sorting option teams.

--- app/views/team/teams.blade.php
+++ app/views/team/teams.blade.php
@@ -72,7 +72,7 @@
 	<script type="text/javascript">
 		jQuery(document).ready(function() {
         $("#myTable")
-        .tablesorter({debug: false, widgets: ['zebra'], sortList: [[0,0]]})
+        .tablesorter({debug: false, widgets: ['zebra'], sortList: [[2,0], [1, 0]]})
         .tablesorterFilter({filterContainer: "#filter-box",
                             filterClearContainer: "#filter-clear-button",
                             filterColumns: [0]}); });
