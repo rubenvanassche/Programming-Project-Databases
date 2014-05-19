5a762444453c2d483b79ebdcad37bee5d030d201
Kristof De Middelaer <kristof.de.middelaer@gmail.com>
2014-05-19 23:18:25 +0200

    Removed test setup.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,8 +17,6 @@ class HomeController extends BaseController {
 
     public function index(){
 
-      Notifications::sendReminders(30);
-
         //require_once('../lib/autoloader.php');
         $recentMatches = Match::getRecentMatches(5);
 	$futureMatches = Match::getFutureMatches(5);
