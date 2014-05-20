786e9466950ad4f284487b15a4aed302f2385f87
JakobStruye <jakob.struye@hotmail.com>
2014-05-20 17:47:22 +0000

    Future matches now show score ? - ? in match overview of team

--- app/views/team/matches.blade.php
+++ app/views/team/matches.blade.php
@@ -11,7 +11,7 @@
 			<tr>
 				<td>{{ $match->date }}</td>
 				<td><a href="{{route('match', array('id'=>$match->match_id))}}">{{ $match->hometeam}} - {{ $match->awayteam }} </a></td>
-				<td><?php echo Match::getScore($match->match_id); ?></td>
+				<td><?php if (Match::isPlayed($match->match_id)) echo Match::getScore($match->match_id); else echo "? - ?" ?></td>
 			</tr>
 		@endforeach					
 	</tbody>
