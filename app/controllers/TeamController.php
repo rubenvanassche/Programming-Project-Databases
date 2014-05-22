e224802b07689197010a602881918707ebdc0b1a
JakobStruye <jakob.struye@hotmail.com>
2014-05-22 00:59:25 +0000

    No news tab if no news available

--- app/controllers/TeamController.php
+++ app/controllers/TeamController.php
@@ -17,12 +17,31 @@ class TeamController extends BaseController {
 			}
 			
 		}
+		//Check if there's news for this team
+		$teamName = $teamObj->name;
+		$articles = RSS::getFIFAtext();
+		$hasNews = false;
+		foreach ($articles as $article){
+			$title = $article->get_title();
+			$description = $article->get_description();
+			
+			$inTitle = strstr($title,$teamName);
+			$inDescription = strstr($description,$teamName);
+			
+			if($inTitle == true or $inDescription == true){
+				$hasNews = true;
+				break;
+			}
+		}
+
 		
-		return View::make('team.team', compact('teamObj', 'teamImageURL', 'teamBackground'))->with('title', $teamObj->name);
+		return View::make('team.team', compact('teamObj', 'teamImageURL', 'teamBackground', 'hasNews'))->with('title', $teamObj->name);
 	}
 	
 	function all(){
 		$teams = Team::getAll();
+
+
 		return View::make('team.teams', compact('teams'))->with('title', 'Teams');
 	}
 	
