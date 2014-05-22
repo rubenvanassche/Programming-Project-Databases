1c5a3e37c0b7d797b6801ea2612a6fcc9e1a9b4c
JakobStruye <jakob.struye@hotmail.com>
2014-05-22 14:59:06 +0000

    Fixed all bets having same teams/date

--- app/views/user/betoverview.blade.php
+++ app/views/user/betoverview.blade.php
@@ -9,34 +9,37 @@
     </ul>
 <div id="myTabContent" class="tab-content">
       <div class="tab-pane fade active in" id="future">
+		<?php $matchIndex = 0; ?>
 		@foreach ($futureBets as $bet)
 			<div class="panel panel-primary" style="margin-top:10px;">
 				<div class="panel-heading">
-					<h3 class="panel-title"><b>{{$bet->hometeam_score}}</b> {{$futureBetsMatches[0]->hometeam}} - {{$futureBetsMatches[0]->awayteam}} <b>{{$bet->awayteam_score}}</b><span class="pull-right">{{$futureBetsMatches[0]->date}}</span></h3>
+					<h3 class="panel-title"><b>{{$bet->hometeam_score}}</b> {{$futureBetsMatches[$matchIndex]->hometeam}} - {{$futureBetsMatches[$matchIndex]->awayteam}} <b>{{$bet->awayteam_score}}</b><span class="pull-right">{{$futureBetsMatches[$matchIndex]->date}}</span></h3>
 				</div>
 				<div class="panel-body">
-					<p><a href="{{ route('team', array('hometeam'=>urlencode($futureBetsMatches[0]->hometeam_id))) }}">{{$futureBetsMatches[0]->hometeam}}</a>: <?php if ($bet->hometeam_reds != -1)echo $bet->hometeam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->hometeam_yellows != -1)echo $bet->hometeam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
-					<p><a href="{{ route('team', array('awayteam'=>urlencode($futureBetsMatches[0]->awayteam_id))) }}">{{$futureBetsMatches[0]->awayteam}}</a>: <?php if ($bet->awayteam_reds != -1)echo $bet->awayteam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->awayteam_yellows != -1)echo $bet->awayteam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
+					<p><a href="{{ route('team', array('hometeam'=>urlencode($futureBetsMatches[$matchIndex]->hometeam_id))) }}">{{$futureBetsMatches[$matchIndex]->hometeam}}</a>: <?php if ($bet->hometeam_reds != -1)echo $bet->hometeam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->hometeam_yellows != -1)echo $bet->hometeam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
+					<p><a href="{{ route('team', array('awayteam'=>urlencode($futureBetsMatches[$matchIndex]->awayteam_id))) }}">{{$futureBetsMatches[$matchIndex]->awayteam}}</a>: <?php if ($bet->awayteam_reds != -1)echo $bet->awayteam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->awayteam_yellows != -1)echo $bet->awayteam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
 					<p><i class="fa fa-circle"></i> First Goal:  <?php if ($bet->first_goal == 0) echo "/";
-									else if ($bet->first_goal == $futureBetsMatches[0]->hometeam_id) echo $futureBetsMatches[0]->hometeam;
-									else echo $futureBetsMatches[0]->awayteam; 
+									else if ($bet->first_goal == $futureBetsMatches[$matchIndex]->hometeam_id) echo $futureBetsMatches[$matchIndex]->hometeam;
+									else echo $futureBetsMatches[$matchIndex]->awayteam; 
 					?></p>
 				</div>
 			</div>
+		<?php $matchIndex = $matchIndex + 1; ?>
 		@endforeach
       </div>
       <div class="tab-pane fade" id="played">
+		<?php $matchIndex = 0; ?>
       	@foreach ($pastBets as $bet)
 			<div class="panel panel-primary" style="margin-top:10px;">
 				<div class="panel-heading">
-					<h3 class="panel-title"><b>{{$bet->hometeam_score}}</b> {{$pastBetsMatches[0]->hometeam}} - {{$pastBetsMatches[0]->awayteam}} <b>{{$bet->awayteam_score}}</b><span class="pull-right">{{$pastBetsMatches[0]->date}}</span></h3>
+					<h3 class="panel-title"><b>{{$bet->hometeam_score}}</b> {{$pastBetsMatches[$matchIndex]->hometeam}} - {{$pastBetsMatches[$matchIndex]->awayteam}} <b>{{$bet->awayteam_score}}</b><span class="pull-right">{{$pastBetsMatches[$matchIndex]->date}}</span></h3>
 				</div>
 				<div class="panel-body">
-					<p><a href="{{ route('team', array('hometeam'=>urlencode($pastBetsMatches[0]->hometeam_id))) }}">{{$pastBetsMatches[0]->hometeam}}</a>: <?php if ($bet->hometeam_reds != -1)echo $bet->hometeam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->hometeam_yellows != -1)echo $bet->hometeam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
-					<p><a href="{{ route('team', array('awayteam'=>urlencode($pastBetsMatches[0]->awayteam_id))) }}">{{$pastBetsMatches[0]->awayteam}}</a>: <?php if ($bet->awayteam_reds != -1)echo $bet->awayteam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->awayteam_yellows != -1)echo $bet->awayteam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
+					<p><a href="{{ route('team', array('hometeam'=>urlencode($pastBetsMatches[$matchIndex]->hometeam_id))) }}">{{$pastBetsMatches[$matchIndex]->hometeam}}</a>: <?php if ($bet->hometeam_reds != -1)echo $bet->hometeam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->hometeam_yellows != -1)echo $bet->hometeam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
+					<p><a href="{{ route('team', array('awayteam'=>urlencode($pastBetsMatches[$matchIndex]->awayteam_id))) }}">{{$pastBetsMatches[$matchIndex]->awayteam}}</a>: <?php if ($bet->awayteam_reds != -1)echo $bet->awayteam_reds; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/redcard.png')}}"/> <?php if ($bet->awayteam_yellows != -1)echo $bet->awayteam_yellows; else echo "0"; ?><img style="height:16px; margin-bottom:7px;" src="{{asset('img/yellowcard.png')}}"/></p>
 					<p><i class="fa fa-circle"></i> First Goal:  <?php if ($bet->first_goal == 0) echo "/";
-									else if ($bet->first_goal == $pastBetsMatches[0]->hometeam_id) echo $pastBetsMatches[0]->hometeam;
-									else echo $pastBetsMatches[0]->awayteam; 
+									else if ($bet->first_goal == $pastBetsMatches[$matchIndex]->hometeam_id) echo $pastBetsMatches[$matchIndex]->hometeam;
+									else echo $pastBetsMatches[$matchIndex]->awayteam; 
 					?></p>
 				</div>
 			</div>
