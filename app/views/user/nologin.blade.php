1a138d5fa2eb482941adb269a446d2702ce075d8
JakobStruye <jakob.struye@hotmail.com>
2014-05-23 01:18:39 +0000

    Improved nologin message

--- app/views/user/nologin.blade.php
+++ app/views/user/nologin.blade.php
@@ -1,5 +1,5 @@
 @section('content')
 <br>
 <br>
-<p style="text-align: center;" >Sorry this content is not available. Please <a href="#" data-toggle="modal" data-target="#loginModal" style="text-decoration: underline;">login</a> or <a href="{{action('UserController@register')}}" style="text-decoration:underline;">register</a>.</p>
+<p style="text-align: center;" >Sorry, this content is only available to logged in users. Please <a href="#" data-toggle="modal" data-target="#loginModal" style="text-decoration: underline;">login</a> or <a href="{{action('UserController@register')}}" style="text-decoration:underline;">register</a>.</p>
 @stop
