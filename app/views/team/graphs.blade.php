adb8075cfd7144b0f4cf24abbf77fcdf3a22a630
JakobStruye <jakob.struye@hotmail.com>
2014-05-23 16:30:35 +0000

    Layout change for graphs

--- app/views/team/graphs.blade.php
+++ app/views/team/graphs.blade.php
@@ -1,6 +1,6 @@
 
     <div id="winloss" style="width: 900px; height: 500px;"></div>
-	<div id="colFilter_div"></div>
-	<div id="chart2_div"></div>
+	<div style="width: 100%; overflow: hidden;"><div id="chart2_div" style="width: 600px; height: 500px; float: left"></div>
+	<div id="colFilter_div"style="margin-left: 620px; width: 200px; height: 200px;"></div></div>
 
 
