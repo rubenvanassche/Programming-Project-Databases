38ce185bcaee1cfd1a32ea6b040700b0143555c8
Tom Roels <tom.roels94@gmail.com>
2014-05-25 22:13:29 +0200

    matches on player page sortable.

--- app/views/player/player.blade.php
+++ app/views/player/player.blade.php
@@ -33,7 +33,7 @@
 		<h3>Information</h3>
 		<p>{{$playerText}}</p>
 		<h3>Goals</h3>
-		<table id="myTable2" class="tablesorter">
+		<table id="myTable" class="tablesorter">
 			<thead>
 				<tr>
 					<th>Date</th>
@@ -54,7 +54,7 @@
 			</tbody>
 		</table>
 		<h3>Cards</h3>
-		<table id="myTable" class="tablesorter">
+		<table id="myTable2" class="tablesorter">
 			<thead>
 				<tr>
 					<th>Date</th>
@@ -112,34 +112,28 @@
 
   <script type="text/javascript">
     jQuery(document).ready(function() {
-        $("#myTable")
+        $("#myTable3")
+        .tablesorter({debug: false, widgets: ['zebra'], sortList: [[0, 1]]})
+        .tablesorterFilter({filterContainer: "#filter-box",
+                            filterClearContainer: "#filter-clear-button",
+                            filterColumns: [0, 1, 2]}); });
+  </script>
+  <script type="text/javascript">
+    jQuery(document).ready(function() {
+        $("#myTable2")
         .tablesorter({debug: false, widgets: ['zebra'], sortList: [[0, 1]]})
         .tablesorterFilter({filterContainer: "#filter-box",
                             filterClearContainer: "#filter-clear-button",
                             filterColumns: [0, 1, 2]}); });
   </script>
 
-  <script src="<?php echo asset('js/tablesorter.js'); ?>" ></script>
-  <script src="<?php echo asset('js/tablesorter_filter.js'); ?>" ></script>
-
     <script type="text/javascript">
     jQuery(document).ready(function() {
-        $("#myTable2")
+        $("#myTable")
         .tablesorter({debug: false, widgets: ['zebra'], sortList: [[0, 1]]})
         .tablesorterFilter({filterContainer: "#filter-box",
                             filterClearContainer: "#filter-clear-button",
-                            filterColumns: [0, 1, 3]}); });
-  </script>
-
-  <script src="<?php echo asset('js/tablesorter.js'); ?>" ></script>
-  <script src="<?php echo asset('js/tablesorter_filter.js'); ?>" ></script>
-
-    <script type="text/javascript">
-    jQuery(document).ready(function() {
-        $("#myTable3")
-        .tablesorter({debug: false, widgets: ['zebra'], sortList: [[0, 1]]})
-        .tablesorterFilter({filterContainer: "#filter-box",
-                            filterClearContainer: "#filter-clear-button" });
+                            filterColumns: [0, 1, 2]}); });
   </script>
 @stop
 
